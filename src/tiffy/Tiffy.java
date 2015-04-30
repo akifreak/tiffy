91aa489089ad873525b31bda76d70f633fe998f6
Lukas Riedersberger <lukasac@gmx.de>
2015-04-30 18:57:41 +0200

    added selection for ffmpeg binary, settings will be saved in the documents folder

--- src/tiffy/Tiffy.java
+++ src/tiffy/Tiffy.java
@@ -1,7 +1,119 @@
+/**
+ * Author: Lukas Riedersberger
+ * License: CC
+ * Free to use, just ask
+ */
+
 package tiffy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Writer;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.Charset;
+
+import javax.swing.*;
 
 public class Tiffy {
 	
-	
+	public static void main(String[] args) {
+		
+		//initialize window
+		JFrame frame = new JFrame("tiffy");  
+        frame.setSize(640,480);
+        frame.setVisible(true);
+		
+		//set path for runtime, create directory with options in homedirectory
+        JFileChooser fr = new JFileChooser();
+        String homedir = fr.getFileSystemView().getDefaultDirectory().getPath();
+
+        File ffmpeg_settings_dir = new File(homedir+"\\tiffy");
+        
+        if(!ffmpeg_settings_dir.exists()){
+        	ffmpeg_settings_dir.mkdir();
+        }
+        
+        File ffmpeg_settings = new File(homedir+"\\tiffy\\settings.ini");
+
+        if(!ffmpeg_settings.exists()) {
+        	try {
+				ffmpeg_settings.createNewFile();
+			} catch (IOException e) {
+				//TODO
+				e.printStackTrace();
+			}      	
+        } 
+        
+        String line;
+        String binary_path = null;
+        //exe defined by [binary]
+        try {
+            InputStream fis = new FileInputStream(homedir+"\\tiffy\\settings.ini");
+            InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+            BufferedReader br = new BufferedReader(isr);
+            
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                if(line.length() >= 10 ){
+                	String compare = line.substring(0, 8);
+                	String tmp = line.substring(line.length()-10, line.length());
+                	if(compare.equals("[binary]") && tmp.equals("ffmpeg.exe")){
+                		binary_path = line.substring(8,line.length());
+                		break;
+                	}
+
+                }
+                
+            }
+        } catch (IOException e) {
+			//TODO
+			e.printStackTrace();
+		}
+
+    	//ask for ffmpeg binary path
+        while(binary_path == null){
+        	System.out.println("binary not found");
+        	JFileChooser pc = new JFileChooser();   
+            pc.setDialogTitle("Select ffmpeg.exe");
+            
+            if(pc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+	        {
+				try {
+					binary_path = pc.getSelectedFile().getCanonicalPath();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	        } 
+            
+            String tmp = binary_path.substring(binary_path.length()-10, binary_path.length());
+            if(!tmp.equals("ffmpeg.exe")){
+            	binary_path = null;
+            	JOptionPane.showMessageDialog(frame, "Please select the ffmpeg.exe");
+            	continue;
+            }
+            
+            Writer output = null;
+            try {
+    			output = new BufferedWriter(new FileWriter(homedir+"\\tiffy\\settings.ini"));
+    	        output.append("[binary]"+binary_path);
+    	        output.close();
+    		} catch (IOException e) {
+    			//TODO
+    			e.printStackTrace();
+    		} 
+        }
+        
+
+        System.out.println(binary_path);
+
+	}
 	
 }
