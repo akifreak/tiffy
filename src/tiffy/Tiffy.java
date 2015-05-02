793002f3d1bde987ce527951042425defd3b906c
Lukas Riedersberger <lukasac@gmx.de>
2015-05-02 12:22:19 +0200

    fixed read out and write out of settings, last used directory is saved

--- src/tiffy/Tiffy.java
+++ src/tiffy/Tiffy.java
@@ -11,6 +11,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,6 +26,109 @@ import javax.swing.*;
 
 
 public class Tiffy {
+	
+	private static String[] getSetting(String file, String code) throws IOException{
+		
+		ArrayList<String> r = new ArrayList<String>();
+		InputStream fis = null;
+
+		fis = new FileInputStream(file);
+
+		InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+		@SuppressWarnings("resource")
+		BufferedReader br = new BufferedReader(isr);
+		String line;
+		String regex = "\\s*\\"+code+"\\b\\s*";
+		while ((line = br.readLine()) != null) {
+			if (line.startsWith(code)) {
+				line = line.replaceFirst(regex,"");
+				r.add(line);
+			} 
+		}
+
+		String[] res = new String[r.size()];
+		for (int i = 0; i < r.size(); ++i){
+			res[i] = r.get(i);
+		}
+		
+		return res;
+	}
+	
+	private static void changeSetting(String file, String code, String option){
+		String line;
+		ArrayList<String> file_list = new ArrayList<String>();
+		InputStream fis = null;
+		try {
+			fis = new FileInputStream(file);
+		} catch (FileNotFoundException e2) {
+			e2.printStackTrace();
+			return;
+		}
+		InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+		@SuppressWarnings("resource")
+		BufferedReader br = new BufferedReader(isr);
+		boolean code_found = false;
+		try {
+			while ((line = br.readLine()) != null) {
+				if (!code_found && line.startsWith(code)) {
+					file_list.add(code+option);
+					code_found = true;
+				} else {
+					file_list.add(line);
+				}
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return;
+		}
+		Writer output = null;
+		try {
+			output = new BufferedWriter(new FileWriter(file));
+			
+			for(int i = 0; i < file_list.size();++i){
+				output.append(file_list.get(i)+"\n");
+			}
+			output.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		} 
+	}
+	
+	private static void appendSetting(String file, String code, String option){
+		String line;
+		ArrayList<String> file_list = new ArrayList<String>();
+		InputStream fis = null;
+		try {
+			fis = new FileInputStream(file);
+		} catch (FileNotFoundException e2) {
+			e2.printStackTrace();
+			return;
+		}
+		InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+		@SuppressWarnings("resource")
+		BufferedReader br = new BufferedReader(isr);
+		try {
+			while ((line = br.readLine()) != null) {
+				file_list.add(line);
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return;
+		}
+		Writer output = null;
+		try {
+			output = new BufferedWriter(new FileWriter(file));
+			
+			for(int i = 0; i < file_list.size();++i){
+				output.append(file_list.get(i)+"\n");
+			}
+			output.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		} 
+	}
 	
 	class PrimeThread extends Thread {
 		BufferedReader stdInput;
@@ -54,8 +158,9 @@ public class Tiffy {
         	ffmpeg_settings_dir.mkdir();
         }
         
-        File ffmpeg_settings = new File(homedir+"\\tiffy\\settings.ini");
-
+        String ffmpeg_settings_path = homedir+"\\tiffy\\settings.ini"; 
+        File ffmpeg_settings = new File(ffmpeg_settings_path);
+        
         if(!ffmpeg_settings.exists()) {
         	try {
 				ffmpeg_settings.createNewFile();
@@ -65,35 +170,25 @@ public class Tiffy {
 			}      	
         } 
         
-        String line;
         String binary_path = null;
-        //exe defined by [binary]
+        String last_dir = null;
         ArrayList<String> codecs = new ArrayList<String>();
-        try {
-            InputStream fis = new FileInputStream(homedir+"\\tiffy\\settings.ini");
-            InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-            @SuppressWarnings("resource")
-			BufferedReader br = new BufferedReader(isr);
-            boolean binary_found = false;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                if(!binary_found && line.length() >= 10 ){
-                	String compare = line.substring(0, 8);
-                	String tmp = line.substring(line.length()-10, line.length());
-                	if(compare.equals("[binary]") && tmp.equals("ffmpeg.exe")){
-                		binary_path = line.substring(8,line.length());
-                		binary_found = true;
-                	}
-                }
-                
-                if(line.startsWith("[codec]")){
-                	String tmp = line.substring(7, line.length());
-                	codecs.add(tmp);
-                }
-                
-            }
+        try {        	
+        	String[] tmp = getSetting(ffmpeg_settings_path,"[binary]");
+        	if(tmp.length >= 1){
+        		binary_path = tmp[0];
+        	}
+        	
+        	tmp = getSetting(ffmpeg_settings_path,"[lastdir]");
+        	if(tmp.length >= 1){
+        		last_dir = tmp[0];
+        	}
+        	
+        	tmp = getSetting(ffmpeg_settings_path,"[codec]");
+        	for(int f = 0; f < tmp.length; ++f)
+        		codecs.add(tmp[f]);
+        	
         } catch (IOException e) {
-			//TODO
 			e.printStackTrace();
 		}
         
@@ -119,30 +214,32 @@ public class Tiffy {
             	continue;
             }
             
-            Writer output = null;
-            try {
-    			output = new BufferedWriter(new FileWriter(homedir+"\\tiffy\\settings.ini"));
-    	        output.append("[binary]"+binary_path);
-    	        output.close();
-    		} catch (IOException e) {
-    			//TODO
-    			e.printStackTrace();
-    		} 
+            changeSetting(ffmpeg_settings_path,"[binary]",binary_path);
+
         }
 
-        System.out.println(binary_path);
+        //System.out.println(binary_path);
         
         frame.setVisible(true);
         
         JFileChooser pc = new JFileChooser();   
         pc.setDialogTitle("Select Movie");
-        
+        if (last_dir != null) pc.setCurrentDirectory(new File(last_dir));        
         String movie = "";
         while(!new File(movie).isFile()){
         	 if(pc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
              {
                  try {
                  	movie = pc.getSelectedFile().getCanonicalPath();
+                 	StringBuilder tmp = new StringBuilder("");
+                 	String[] parts = movie.split("\\\\");
+                 	for(int i = 0; i < parts.length-1; ++i){
+                 		tmp.append(parts[i]+"\\");
+                 	} last_dir = tmp.toString();
+                 	
+                 	changeSetting(ffmpeg_settings_path,"[lastdir]",last_dir);
+
+                 	
      			} catch (IOException e) {
      				e.printStackTrace();
      			}
@@ -158,9 +255,6 @@ public class Tiffy {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-        /*BufferedReader stdInput = new BufferedReader(new 
-             InputStreamReader(proc.getInputStream()));*/
 
         BufferedReader stdError = new BufferedReader(new 
              InputStreamReader(proc.getErrorStream()));
